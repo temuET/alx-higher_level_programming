472ee23403c2efdc07fc25b869bcf1464e58a422
temuET <ttemu2016@gmail.com>
2022-09-19 18:52:42 -0700

    hot fix

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[113:118] + " " + str[0:6]
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[107:113] + str[:6]
 print(str)
